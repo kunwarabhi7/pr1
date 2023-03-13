1afe1d8b743c240d3fa2a1324e411348ef8ab7a9
Abhishek <abhishekparmar162@gmail.com>
2023-03-13 17:50:39 +0530

    added img

--- next.config.js
+++ next.config.js
@@ -1,12 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
 module.exports = {
   images: {
-    domains: ['images.unsplash.com','plus.unsplash.com','content3.jdmagicbox.com','content.jdmagicbox.com'],
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "content3.jdmagicbox.com",
+      "content.jdmagicbox.com",
+      "i0.wp.com",
+      "www.avantdental.in",
+      "johnscreekdentalexcellence.com",
+      "www.kidsandteenspediatricdentalcare.com",
+      "www.definitivedental.com"
+    ],
   },
-}
+};
 
 // 06271000105182
