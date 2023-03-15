6b69a4c30059c0aac7a3e1bfe92d36e5bf68abd5
Abhishek <abhishekparmar162@gmail.com>
2023-03-15 12:05:21 +0530

    fix css

--- components/Home/Services.jsx
+++ components/Home/Services.jsx
@@ -5,7 +5,7 @@ const Services = ({ imageUrl, title }) => {
   return (
     <div className="max-w-sm mx-auto  overflow-hidden shadow-lg rounded-lg cursor-pointer transition ease-in-out delay-150 hover:translate-y-1 hover:scale-105 duration-200">
     <div className="relative h-48 w-96">
-      <Image className="object-cover rounded-xl object-center w-full h-full" src={imageUrl} fill alt={title} />
+      <Image className="object-fit rounded-xl object-center w-full h-full" src={imageUrl} fill alt={title} />
     </div>
     <div className="px-6 py-4 md:p-6">
       <div className="font-bold text-xl  mb-2">{title}</div>
